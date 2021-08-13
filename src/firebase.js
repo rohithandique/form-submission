6197275a254d0f50a4113b283688c31f22ec9015
Rohit Handique <rohit.handique.rh@gmail.com>
2021-08-13 17:42:14 +0530

    changed firebase.js to public

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyBGnFKIFcMP24Sse6t1ehlUUrKagDgN0L0",
+    authDomain: "fir-auth-183aa.firebaseapp.com",
+    projectId: "fir-auth-183aa",
+    storageBucket: "fir-auth-183aa.appspot.com",
+    messagingSenderId: "104082318288",
+    appId: "1:104082318288:web:1817e5434d1c68da91cfd5"
+})
+
+export const auth = app.auth()
+export default app;
